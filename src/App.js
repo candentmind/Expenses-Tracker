5d49fb1f54953d27eac330182b857181bdc59c64
CandentMind <efe.emma@gmail.com>
2022-10-30 05:04:23 +0100

    Update DUMMY_EXPENSES data

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ import NewExpense from "./components/NewExpense/NewExpense";
 const DUMMY_EXPENSES = [
   {
     id: "e1",
-    title: "Toilet Paper",
+    title: "Toiletries",
     amount: 94.12,
     date: new Date(2020, 7, 14),
   },
   {
     id: "e3",
-    title: "Car Insurance",
+    title: "Health Insurance",
     amount: 294.67,
     date: new Date(2021, 2, 28),
   },
@@ -21,7 +21,15 @@ const DUMMY_EXPENSES = [
     amount: 450,
     date: new Date(2021, 5, 12),
   },
+  {
+    id: "e5",
+    title: "Microbit",
+    amount: 5000,
+    date: new Date(2022, 9, 15),
+  },
 ];
+
+console.log(DUMMY_EXPENSES);
 
 const App = () => {
   const useSemiPersistentState = (
@@ -62,14 +70,14 @@ const App = () => {
     });
   };
   const getSelectedExpense = (expenseId) => {
-    const expenseItem = expenses.find(item => item.id === expenseId);
+    const expenseItem = expenses.find((item) => item.id === expenseId);
     console.log(expenseItem);
-  }
+  };
 
   return (
     <div>
       <NewExpense onAddExpense={addExpense} />
-      <ExpensesPanel items={expenses} onEditExpense={getSelectedExpense}/>
+      <ExpensesPanel items={expenses} onEditExpense={getSelectedExpense} />
     </div>
   );
 };
